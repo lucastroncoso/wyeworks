2581418e2fa6506285836772a795147238e8cf4c
lucastroncoso <lucastroncoso.seguros@gmail.com>
2022-09-03 17:37:07 +0200

    Add .env example

---
+++ config/example.dev.secret.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :discography,
+  trello_api_key: "xxxxxxxxxxxxxx",
+  trello_api_token: "xxxxxxxxxxxxxx",
+  spotify_client_id: "xxxxxxxxxxxxxx",
+  spotify_client_secret: "xxxxxxxxxxxxxx"
